10a0cf14647c6393b77003cf034fe0dfcf162cc5
dhimsrasyad <63093884+dhimsrasyad@users.noreply.github.com>
2021-09-22 22:12:42 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Home from "./Components/Home";
 import Mobil from "./Components/Mobil";
 
 export default function App() {
@@ -10,13 +11,17 @@ export default function App() {
           <p>Kelompok 11</p>
         </div>
         <nav>
+          <Link className="text" to="/Home">
+            Homeㅤ
+          </Link>
           <Link className="text" to="/Mobil">
             Daftar Mobil
           </Link>
         </nav>
       </header>
       <Switch>
-        <Route exact path="/" component={Mobil} />
+        <Route exact path="/" component={Home} />
+        <Route path="/Home" exact component={Home} />
         <Route path="/Mobil" exact component={Mobil} />
       </Switch>
     </BrowserRouter>
